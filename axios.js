9c023400d283e6b478b0de0ce81658916d8ce760
_YinQ <1339529465@qq.com>
2022-01-06 16:07:56 +0800

    Update axios.js

--- axios.js
+++ axios.js
@@ -1,1 +1,17 @@
 
+import axios from 'axios'
+
+const requests = axios.create({
+    baseURL: '/api',
+    timeout: 5000
+})
+
+requests.interceptors.request.use(config => config)
+
+requests.interceptors.request.use((res) => {
+    return res.data;
+},(error)=>{
+    return Promise.reject(new error('faile'));
+})
+
+export default axios;
